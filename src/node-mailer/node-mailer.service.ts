19d4f80a6bdc61add834c84483b6b2ff45ae90d2
Kittonn <tonkittipod@gmail.com>
2023-11-09 06:52:38 +0700

    refactor: refactor code

--- src/node-mailer/node-mailer.service.ts
+++ src/node-mailer/node-mailer.service.ts
@@ -8,13 +8,6 @@ export class NodeMailerService {
   constructor(private readonly mailerService: MailerService) {}
 
   async sendEmail(emailOptions: EmailOptions): Promise<void> {
-    console.log(__dirname)
-    await this.mailerService.sendMail({
-      to: emailOptions.to,
-      subject: emailOptions.subject,
-      // template: path.join(__dirname, './templates', emailOptions.template),
-      template: emailOptions.template,
-      context: emailOptions.context,
-    });
+    await this.mailerService.sendMail(emailOptions);
   }
 }
