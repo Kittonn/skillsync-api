be046fb653419db3f5809eaef302d4baea2a1c6a
Kittonn <tonkittipod@gmail.com>
2023-11-20 23:58:15 +0700

    feat: update user service

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -11,10 +11,10 @@ import { UsersService } from './users.service';
 import { AccessTokenGuard } from '@/common/guards/access-token.guard';
 import { GetUser } from '@/common/decorators/get-user.decorator';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { User } from '@prisma/client';
 import { ChangePasswordDto } from './dto/change-password.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { FileValidationPipe } from '@/common/pipes/file-validation.pipe';
+import { User } from './schema/user.schema';
 
 @Controller('users')
 @UseGuards(AccessTokenGuard)
@@ -22,38 +22,38 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('/me')
-  async getUserInfo(@GetUser('id') userId: string): Promise<User> {
+  async getUserInfo(@GetUser('_id') userId: string): Promise<User> {
     return this.usersService.getUserInfo(userId);
   }
 
-  // @Put()
-  // async updateUserInfo(
-  //   @GetUser('id') userId: string,
-  //   @Body() updateUserDto: UpdateUserDto,
-  // ): Promise<User> {
-  //   return this.usersService.updateUserInfo(userId, updateUserDto);
-  // }
+  @Put()
+  async updateUserInfo(
+    @GetUser('_id') userId: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ): Promise<User> {
+    return this.usersService.updateUserInfo(userId, updateUserDto);
+  }
 
-  // @Put('/change-password')
-  // async updatePassword(
-  //   @GetUser() user: User,
-  //   @Body() changePasswordDto: ChangePasswordDto,
-  // ): Promise<User> {
-  //   return this.usersService.updatePassword(user, changePasswordDto);
-  // }
+  @Put('/change-password')
+  async updatePassword(
+    @GetUser() user: User,
+    @Body() changePasswordDto: ChangePasswordDto,
+  ): Promise<User> {
+    return this.usersService.updatePassword(user, changePasswordDto);
+  }
 
-  // @Put('/change-avatar')
-  // @UseInterceptors(FileInterceptor('file'))
-  // async updateAvatar(
-  //   @UploadedFile(
-  //     new FileValidationPipe({
-  //       maxSize: 10 * 1024 * 1024,
-  //       fileType: /^image\/(png|jpeg|jpg|webp)$/,
-  //     }),
-  //   )
-  //   file: Express.Multer.File,
-  //   @GetUser('id') userId: string,
-  // ): Promise<User> {
-  //   return this.usersService.updateAvatar(file, userId);
-  // }
+  @Put('/change-avatar')
+  @UseInterceptors(FileInterceptor('file'))
+  async updateAvatar(
+    @UploadedFile(
+      new FileValidationPipe({
+        maxSize: 10 * 1024 * 1024,
+        fileType: /^image\/(png|jpeg|jpg|webp)$/,
+      }),
+    )
+    file: Express.Multer.File,
+    @GetUser('_id') userId: string,
+  ): Promise<User> {
+    return this.usersService.updateAvatar(file, userId);
+  }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -3,8 +3,8 @@ import { UsersRepository } from './users.repository';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { RedisService } from '@/database/redis/redis.service';
 import { ChangePasswordDto } from './dto/change-password.dto';
-import { User } from '@prisma/client';
 import { compare, hash } from '@/shared/utils/encrypt';
+import { User } from './schema/user.schema';
 import { CloudinaryService } from '@/database/cloudinary/cloudinary.service';
 
 @Injectable()
@@ -23,67 +23,67 @@ export class UsersService {
     return userInfo as User;
   }
 
-  // async updateUserInfo(
-  //   userId: string,
-  //   updateUserDto: UpdateUserDto,
-  // ): Promise<User> {
-  //   const user = await this.usersRepository.update({
-  //     where: { id: userId },
-  //     data: updateUserDto,
-  //   });
+  async updateUserInfo(
+    userId: string,
+    updateUserDto: UpdateUserDto,
+  ): Promise<User> {
+    const user = await this.usersRepository.update(
+      { _id: userId },
+      updateUserDto,
+    );
 
-  //   await this.redisService.set(userId, JSON.stringify(user));
+    await this.redisService.set(userId, JSON.stringify(user));
 
-  //   const { password, refreshToken, ...userInfo } = user;
-  //   return userInfo as User;
-  // }
+    const { password, refreshToken, ...userInfo } = user['_doc'];
+    return userInfo as User;
+  }
 
-  // async updatePassword(
-  //   user: User,
-  //   changePasswordDto: ChangePasswordDto,
-  // ): Promise<User> {
-  //   const isPasswordMatch = await compare(
-  //     changePasswordDto.oldPassword,
-  //     user.password,
-  //   );
+  async updatePassword(
+    user: User,
+    changePasswordDto: ChangePasswordDto,
+  ): Promise<User> {
+    const isPasswordMatch = await compare(
+      changePasswordDto.oldPassword,
+      user.password,
+    );
 
-  //   if (!isPasswordMatch) {
-  //     throw new UnauthorizedException('Invalid credentials');
-  //   }
+    if (!isPasswordMatch) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
 
-  //   const hashedPassword = await hash(changePasswordDto.newPassword);
+    const hashedPassword = await hash(changePasswordDto.newPassword);
 
-  //   const updatedUser = await this.usersRepository.update({
-  //     where: { id: user.id },
-  //     data: { password: hashedPassword },
-  //   });
+    const updatedUser = await this.usersRepository.update(
+      { _id: user._id },
+      { password: hashedPassword },
+    );
 
-  //   await this.redisService.set(user.id, JSON.stringify(updatedUser));
+    await this.redisService.set(user._id, JSON.stringify(updatedUser));
 
-  //   const { password, refreshToken, ...userInfo } = updatedUser;
-  //   return userInfo as User;
-  // }
+    const { password, refreshToken, ...userInfo } = updatedUser['_doc'];
+    return userInfo as User;
+  }
 
-  // async updateAvatar(file: Express.Multer.File, userId: string): Promise<User> {
-  //   const user = await this.usersRepository.findOne({ id: userId });
+  async updateAvatar(file: Express.Multer.File, userId: string): Promise<User> {
+    const user = await this.usersRepository.findOne({ id: userId });
 
-  //   const uploadedFile = await this.cloudinaryService.uploadFile(file);
-  //   const avatarData = {
-  //     publicId: uploadedFile.public_id,
-  //     url: uploadedFile.secure_url,
-  //   };
+    const uploadedFile = await this.cloudinaryService.uploadFile(file);
+    const avatarData = {
+      publicId: uploadedFile.public_id,
+      url: uploadedFile.secure_url,
+    };
 
-  //   if (user.avatar?.publicId) {
-  //     await this.cloudinaryService.deleteFile(user.avatar.publicId);
-  //   }
+    if (user?.avatar) {
+      await this.cloudinaryService.deleteFile(user.avatar.publicId);
+    }
 
-  //   const updatedUser = await this.usersRepository.update({
-  //     where: { id: userId },
-  //     data: { avatar: avatarData },
-  //   });
+    const updatedUser = await this.usersRepository.update(
+      { _id: userId },
+      { avatar: avatarData },
+    );
 
-  //   await this.redisService.set(userId, JSON.stringify(updatedUser));
-  //   const { password, refreshToken, ...userInfo } = updatedUser;
-  //   return userInfo as User;
-  // }
+    await this.redisService.set(userId, JSON.stringify(updatedUser));
+    const { password, refreshToken, ...userInfo } = updatedUser['_doc'];
+    return userInfo as User;
+  }
 }
